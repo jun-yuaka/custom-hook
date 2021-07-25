82a6e06b68f366a1d3015bf83fb8efcb3e08067e
jun-yuaka <85206734+jun-yuaka@users.noreply.github.com>
2021-07-25 16:27:38 +0900

    data取得してmapで展開して表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import "./styles.css";
 import { UserCard } from "./compornents/UserCard";
+import axios from "axios";
+import { User } from "./types/api/user";
+import { useState } from "react";
+import { UserProfile } from "./types/UserProfile";
 
 const user = {
   id: 1,
@@ -9,9 +13,28 @@ const user = {
 };
 
 export default function App() {
+  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
+
+  const onClickFetchUser = () => {
+    axios
+      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        const data = res.data.map((user) => ({
+          id: user.id,
+          name: `${user.name}(${user.username})`,
+          email: user.email,
+          address: `${user.address.city}${user.address.suite}${user.address.city}`
+        }));
+        setUserProfiles(data);
+      });
+  };
+
   return (
     <div className="App">
-      <UserCard user={user} />
+      <button onClick={onClickFetchUser}>データ取得</button>
+      {userProfiles.map((user) => (
+        <UserCard user={user} />
+      ))}
     </div>
   );
 }
