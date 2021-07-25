0a468105011fa9f1ceccb0aae04a1c0a1864ea51
jun-yuaka <85206734+jun-yuaka@users.noreply.github.com>
2021-07-25 13:53:55 +0900

    カード情報のサンプルを一つ用意

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import "./styles.css";
+import { UserCard } from "./compornents/UserCard";
+
+const user = {
+  id: 1,
+  name: "jan",
+  email: "test@aa.com",
+  address: "ADDRES"
+};
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <UserCard user={user} />
     </div>
   );
 }
